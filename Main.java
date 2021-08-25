b117d5b662aea96d0e45630569200e18950d808a
RedstoneNotPlace <79918051+RedstoneNotPlaced@users.noreply.github.com>
2021-08-25 20:53:54 +0800

    Changed variables
    I changed the variables at long i and long i2 and changed it into l and l2.

--- Main.java
+++ Main.java
@@ -12,23 +12,23 @@ public class Main {
         System.out.println("-----------------------");
         System.out.println("");
         System.out.println("Enter a number: ");
-        long i = scanner.nextLong();
+        long l = scanner.nextLong();
         System.out.println("Enter a second number: ");
-        long i2 = scanner.nextLong();
+        long l2 = scanner.nextLong();
         System.out.println("Choose what operator to use: ");
         String operator = scanner.next();
         switch (operator) {
             case "+":
-                System.out.println("The sum is: " + Math.addExact(i, i2));
+                System.out.println("The sum is: " + Math.addExact(l, l2));
                 break;
             case "-":
-                System.out.println("The difference is: " + Math.subtractExact(i, i2));
+                System.out.println("The difference is: " + Math.subtractExact(l, l2));
                 break;
             case "*":
-                System.out.println("The product is: " + Math.multiplyExact(i, i2));
+                System.out.println("The product is: " + Math.multiplyExact(l, l2));
                 break;
             case "/":
-                System.out.println("The quotient is: " + Math.floorDiv(i, i2));
+                System.out.println("The quotient is: " + Math.floorDiv(l, l2));
                 break;
             default:
                 System.out.println("Thats not a valid operator! Here's a list of operators:");
